069f2eec816602fee4bf033f0be9d407de76eedc
”竹内幹人” <orzmktmkt310@gmail.com>
2019-08-02 03:22:45 +0000

    05

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # helper_method使用により削除
+    #def current_user
+      #@current_user ||= User.find_by(id: session[:user_id])
+    #end
+
+    #def logged_in?
+      #!current_user.nil?
+    #end
 end
--- db/migrate/20190725082805_add_password_digest_to_user.rb
+++
@@ -1,4 +0,0 @@
-class AddPasswordDigestToUser < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
